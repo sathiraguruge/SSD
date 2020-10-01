eaa6f835910d760f52f3eb7c6d0971665b9e7b65
Sathira Guruge <it17042352@my.sliit.lk>
2020-10-01 16:14:06 +0530

    Upload function done

--- code/backend/index.js
+++ code/backend/index.js
@@ -1,59 +1,151 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const passport = require('passport');
-const cookieSession = require('cookie-session')
-require('./playtech-credentials');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { google } = require('googleapis');
+const fs = require("fs");
+const formidable = require('formidable');
+const credentials = require('./playtech-credentials.json');
 
-app.use(cors());
+const client_id = credentials.web.client_id;
+const client_secret = credentials.web.client_secret;
+const redirect_uris = credentials.web.redirect_uris;
+const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+const SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file']
 
-// parse application/json
-app.use(bodyParser.json())
-
-// For an actual app you should configure this with an experation time, better keys, proxy and secure
-app.use(cookieSession({
-    name: 'play-tech-session',
-    keys: ['key1', 'key2']
-}))
-
-// Auth middleware that checks if the user is logged in
-const isLoggedIn = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.sendStatus(401);
-    }
+var corsOptions = {
+    origin: 'http://localhost:3010',
+    optionsSuccessStatus: 200 ,// For legacy browser support
+    methods: "GET, POST"
 }
 
-// Initializes passport and passport sessions
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Example protected and unprotected routes
-app.get('/', (req, res) => res.send('Example Home page!'))
-app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+app.get('/', (req, res) => res.send(' API Running'));
 
-// In this route you can see that if the user is logged in u can acess his info in: req.user
-app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
+app.get('/getAuthURL', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPE,
+    });
+    console.log(authUrl);
+    return res.send(authUrl);
+});
 
-// Auth Routes
-app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.post('/getToken', (req, res) => {
+    if (req.body.code == null) return res.status(400).send('Invalid Request');
+    oAuth2Client.getToken(req.body.code, (err, token) => {
+        if (err) {
+            console.error('Error retrieving access token', err);
+            return res.status(400).send('Error retrieving access token');
+        }
+        res.send(token);
+    });
+});
 
-app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/good');
-    }
-);
+app.post('/getUserInfo', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
 
-app.get('/logout', (req, res) => {
-    req.session = null;
-    req.logout();
-    res.redirect('/');
-})
+    oauth2.userinfo.get((err, response) => {
+        if (err) res.status(400).send(err);
+        console.log(response.data);
+        res.send(response.data);
+    })
+});
 
-app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
+app.post('/readDrive', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    drive.files.list({
+        pageSize: 10,
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return res.status(400).send(err);
+        }
+        const files = response.data.files;
+        if (files.length) {
+            console.log('Files:');
+            files.map((file) => {
+                console.log(`${file.name} (${file.id})`);
+            });
+        } else {
+            console.log('No files found.');
+        }
+        res.send(files);
+    });
+});
+
+app.post('/fileUpload', (req, res) => {
+    var form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if (err) return res.status(400).send(err);
+        const token = JSON.parse(fields.token);
+        console.log(token)
+        if (token == null) return res.status(400).send('Token not found');
+        oAuth2Client.setCredentials(token);
+        console.log(files.file);
+        const drive = google.drive({ version: "v3", auth: oAuth2Client });
+        const fileMetadata = {
+            name: files.file.name,
+        };
+        const media = {
+            mimeType: files.file.type,
+            body: fs.createReadStream(files.file.path),
+        };
+        console.log('Test')
+
+        drive.files.create(
+            {
+                resource: fileMetadata,
+                media: media,
+                fields: "id",
+            },
+            (err, file) => {
+                oAuth2Client.setCredentials(null);
+                if (err) {
+                    console.error(err);
+                    res.status(400).send(err)
+                } else {
+                    res.send('Successful')
+                }
+            }
+        );
+    });
+});
+
+app.post('/deleteFile/:id', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    var fileId = req.params.id;
+    drive.files.delete({ 'fileId': fileId }).then((response) => { res.send(response.data) })
+});
+
+app.post('/download/:id', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    var fileId = req.params.id;
+    drive.files.get({ fileId: fileId, alt: 'media' }, { responseType: 'stream' },
+        function (err, response) {
+            response.data
+                .on('end', () => {
+                    console.log('Done');
+                })
+                .on('error', err => {
+                    console.log('Error', err);
+                })
+                .pipe(res);
+        });
+
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Started ${PORT}`));
