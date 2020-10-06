538bdb67de0f2b5e8e6b08e057f824f726954426
nirmaniPathiranage <nirma2015@gmail.com>
2020-10-06 11:56:04 +0530

    comments added for Driver Routing

--- code/backend/routing/DriveRouting.js
+++ code/backend/routing/DriveRouting.js
@@ -1,7 +1,9 @@
+//add relevant imports
 const express = require('express');
 const router = express.Router();
 const DriveController = require('../controller/DriveController');
 
+//post request to login page
 router.post('/', (req, res) => {
     DriveController.createFolder(req).then((data) => {
         res.json(data);
