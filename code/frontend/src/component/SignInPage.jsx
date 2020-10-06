d8602442bfa52ef61dd45ba609d479e030502a12
nirmaniPathiranage <nirma2015@gmail.com>
2020-10-06 10:51:26 +0530

    comments added for sign in

--- code/frontend/src/component/SignInPage.jsx
+++ code/frontend/src/component/SignInPage.jsx
@@ -1,3 +1,4 @@
+//add the relevant imports
 import React from 'react';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Avatar from "@material-ui/core/Avatar";
@@ -7,43 +8,49 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
-import {withAlert} from 'react-alert'
+import { withAlert } from 'react-alert'
 import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import GoogleLogin from "react-google-login";
 
+//initialize SignInPage component
 class SignInPage extends React.Component {
     constructor(props) {
         super(props);
         this.onChange = this.onChange.bind(this);
     }
 
+    // on change state
     onChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         });
     }
 
+    //render component
     render() {
+        //giving alert for success or failure
         const alert = this.props.alert;
         return (
             <Container component="main" maxWidth="xs">
-                <CssBaseline/>
+                <CssBaseline />
                 <div className="paper">
-                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <Avatar style={{
                             marginTop: "30px"
                         }}>
-                            <LockOutlinedIcon/>
+                            <LockOutlinedIcon />
                         </Avatar>
                     </div>
 
+                    {/* Sign in component */}
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
                     <form className="" noValidate>
+                        {/* Email verification */}
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -55,6 +62,7 @@ class SignInPage extends React.Component {
                             autoComplete="email"
                             autoFocus
                         />
+                        {/* Password verification */}
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -67,7 +75,7 @@ class SignInPage extends React.Component {
                             autoComplete="current-password"
                         />
                         <FormControlLabel
-                            control={<Checkbox value="remember" color="primary"/>}
+                            control={<Checkbox value="remember" color="primary" />}
                             label="Remember me"
                         />
                         <Button
@@ -83,19 +91,28 @@ class SignInPage extends React.Component {
                         <div style={{
                             display: 'flex', justifyContent: 'center', margin: "30px"
                         }}>
+                            {/* Google Login component */}
                             <GoogleLogin
+                                //initialise the client id from Google developer console
                                 clientId="921424005912-gto8ohqq1vkihpjsjrpvbre81efhtntd.apps.googleusercontent.com"
                                 buttonText="Sign in with Google"
+                                //asking for email, profile and openid scopes access
                                 scope={"https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file"}
+                                //success
                                 onSuccess={(response) => {
+                                    //setting the access token with the reveived JSON object
                                     localStorage.setItem('Token', JSON.stringify(response.tokenObj));
+                                    //setting the profile with the reveived JSON object
                                     localStorage.setItem('Profile', JSON.stringify(response.profileObj));
+                                    //Display the name of the user
                                     alert.info('Hello ' + response.profileObj.name, {
                                         onClose: () => {
+                                            //access the protected resource
                                             window.location.href = "/homepage"
                                         }
                                     })
                                 }}
+                                //failure
                                 onFailure={() => {
                                     alert.error('Login Failed !')
                                 }}
@@ -103,6 +120,7 @@ class SignInPage extends React.Component {
                             />
                         </div>
 
+                        {/* Forget Password component */}
                         <Grid container>
                             <Grid item xs>
                                 <Link href="#" variant="body2">
