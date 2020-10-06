c2dbc9a258417a50aa6b732b49a02dd58aeddae4
nirmaniPathiranage <nirma2015@gmail.com>
2020-10-06 11:08:25 +0530

    comments added for Google Service

--- code/frontend/src/service/GoogleService.js
+++ code/frontend/src/service/GoogleService.js
@@ -1,22 +1,31 @@
+//add relevant imports
 import APIService from './APIService'
 
+//initialize Google Service class in order to access GData
 export default class GoogleService {
+    //default constructor
     constructor() {
         this.apiService = new APIService();
     }
 
+    //file upload functionality
     uploadFile(file) {
+        //getting access token generated according to the authorization request
         const token = localStorage.getItem('Token');
         const h = {}; //headers
         let data = new FormData();
+        //token
         data.append('token', token);
+        //image file to be uploaded
         data.append('file', file);
         h.Accept = 'application/json'; //if you expect JSON response
 
+        //post the JSON object with token and image file to the specified URL
         fetch('http://localhost:3000/api/drive', {
             method: 'POST',
             headers: h,
             body: data
+            // success message
         }).then(response => {
             alert('Image Added Successfully')
         }).catch(err => {
